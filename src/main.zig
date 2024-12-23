81a7321961a2dfb755f3d09dc82939b247fa6bd9
CaiNiaoE <rookieyi@outlook.com>
2024-12-23 15:22:51 +0800

    [main]feat: add edit command

--- src/main.zig
+++ src/main.zig
@@ -6,15 +6,16 @@ pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
-
     var args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
     var env = try std.process.getEnvMap(allocator);
     defer env.deinit();
+
     const repositories = env.get("US_REPOSITORIES") orelse {
         std.debug.print("Error:Enverment US_REPOSITORIES not found\n", .{});
         return error.RepositoriesNotSet;
     };
+    const editor = env.get("EDITOR") orelse "nano";
     if (args.len < 2) {
         try printUsage();
         return;
@@ -25,6 +26,15 @@ pub fn main() !void {
         try listScripts(repositories);
     } else if (mem.eql(u8, command, "ls-exe")) {
         try listExecutor();
+    } else if (mem.eql(u8, command, "edit")) {
+        if (args.len < 3) {
+            std.debug.print("Error:Script name not found\n", .{});
+            try printUsage();
+            return;
+        }
+        const script_found = try getScript(allocator, repositories, args[2]);
+        try editScript(allocator, script_found, editor);
+        allocator.free(script_found);
     } else if (mem.eql(u8, command, "run")) {
         if (args.len < 3) {
             std.debug.print("Error:Script name not found\n", .{});
@@ -33,7 +43,19 @@ pub fn main() !void {
         }
         const script_found = try getScript(allocator, repositories, args[2]);
         try exectuor.run(allocator, script_found, args[3..]);
+        allocator.free(script_found);
     }
+}
+fn editScript(allocator: mem.Allocator, full_path: []const u8, editor: []const u8) !void {
+    var full_args = std.ArrayList([]const u8).init(allocator);
+    defer full_args.deinit();
+
+    try full_args.append(editor);
+    try full_args.append(full_path);
+    var child = std.process.Child.init(full_args.items, allocator);
+    child.stderr_behavior = .Inherit;
+    child.stdout_behavior = .Inherit;
+    _ = try child.spawnAndWait();
 }
 fn listScripts(repo: []const u8) !void {
     var dir = try std.fs.openDirAbsolute(repo, .{ .iterate = true });
@@ -63,9 +85,10 @@ fn printUsage() !void {
         \\Union-Script
         \\
         \\Usage：
-        \\  us list         - list all script
+        \\  us list           - list all script
         \\  us ls-exe         - list all executor
-        \\  us run <script> - run script
+        \\  us edit           - edit script
+        \\  us run <script>   - run script
         \\
         \\env：
         \\  US_REPOSITORIES - script repositories
